985d61db6f2ed91c3602403ade00779b4f30f565
A. Y. Firdaus <amyar32@gmail.com>
2021-04-21 13:42:58 +0700

    remove unused component

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Hisab from "./components/Hisab";
 import JadwalList from "./components/JadwalList";
-import DarkModeButton from "./components/DarkModeButton";
 
 const app = createApp(App);
 
 app.component("nav-bar", Navbar);
 app.component("hisab", Hisab);
 app.component("jadwal-list", JadwalList);
-app.component("dark-toggler", DarkModeButton);
 
 app.mount("#app");
